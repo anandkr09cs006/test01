17884a7a8ba5d7eebe8b4239bf5d63e2688e8de2
anandkr09cs006 <96418564+anandkr09cs006@users.noreply.github.com>
2022-03-11 22:25:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+provider "google" {
+# Please put SA accout JSON Key file in place of XXXX
+  credentials = "${file("XXXXXXXXXXXXXXXX")}"
+  project = "[Your_Anand_Name]"//proj name 
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+ network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
